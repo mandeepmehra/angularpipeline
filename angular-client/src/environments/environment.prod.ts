990d56ef9c82da36cf7e89807741a70f7318f982
Priyanku Konar <konar.priyanku@gmail.com>
2019-06-03 17:17:34 +0530

    updated nginx configuration for api reverse proxy

--- angular-client/src/environments/environment.prod.ts
+++ angular-client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 const API_HOST='kworker1'; //use the kubernetes node name 
-const API_PORT='31199'; //use the kubernetes nodeport on which api service  is running 
+const API_PORT='30014'; //use the kubernetes nodeport on which api service  is running 
 export const environment = {
   production: true,
 
